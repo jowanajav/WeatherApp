9f8f214523e65b17efc8b51fb91fdf890b5a9fee
jowanajav <72789389+jowanajav@users.noreply.github.com>
2021-09-07 09:43:51 +0900

    Added default city

--- src/script.js
+++ src/script.js
@@ -25,6 +25,8 @@ function formatDate(timestamp) {
 
 function searchData(response) {
   console.log(response);
+  let city = response.data.name;
+  let displayCity = document.querySelector("#disp-city");
   let country = response.data.sys.country;
   let displayCountry = document.querySelector("#disp-country");
   let date = formatDate(response.data.dt);
@@ -45,6 +47,7 @@ function searchData(response) {
   let displayWind = document.querySelector("#wind");
   let iconData = document.querySelector("#main-icon");
 
+  displayCity.innerHTML = `${city}`;
   displayCountry.innerHTML = `${country}`;
   displayDate.innerHTML = `${date}`;
   weatherDescription.innerHTML = `${cityWeather}`;
@@ -60,28 +63,18 @@ function searchData(response) {
   );
 }
 
-function searchCity(event) {
-  event.preventDefault();
+function searchCity(city) {
   let apiKey = "471a6be89778a7f92e1728b2754d799c";
-  let inputCity = document.querySelector("#input-city");
-  let cityValue = inputCity.value;
-  let inputCapitalized = cityValue.split(" ");
-  let searchedCity = document.querySelector("#disp-city");
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-  let apiUrl = `${apiEndpoint}?q=${inputCapitalized}&appid=${apiKey}&units=metric`;
-
-  for (let i = 0; i < inputCapitalized.length; i++) {
-    inputCapitalized[i] =
-      inputCapitalized[i][0].toUpperCase() + inputCapitalized[i].substr(1);
-  }
-
-  if (document.getElementById("input-city").value.length == 0) {
-    searchedCity.innerHTML = `Invalid City `;
-  } else {
-    searchedCity.innerHTML = `${inputCapitalized.join(" ")} `;
-  }
+  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(searchData);
+}
+
+function handleCity(event) {
+  event.preventDefault();
+  let searchedCity = document.querySelector("#input-city");
+  searchCity(searchedCity.value);
 }
 
 function currentCity(response) {
@@ -108,8 +101,10 @@ function getCurrentLocation(event) {
   navigator.geolocation.getCurrentPosition(getPosition);
 }
 
-let searchButton = document.querySelector("#search-city");
-searchButton.addEventListener("click", searchCity);
+let searchButton = document.querySelector("#search-form");
+searchButton.addEventListener("click", handleCity);
 
 let currentLocButton = document.querySelector("#current-loc");
 currentLocButton.addEventListener("click", getCurrentLocation);
+
+searchCity(`Manila`);
